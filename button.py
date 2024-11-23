1bc31d8b55499360903733dbef5efc366ec314a2
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-11-23 22:53:02 +0530

    Delete button.py

--- button.py
+++
@@ -1,8 +0,0 @@
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-def rename_button(file_name, file_extension):
-    buttons = [
-        [InlineKeyboardButton("Rename", callback_data=f"rename:{file_name}:{file_extension}")],
-        [InlineKeyboardButton("Cancel", callback_data="cancel")]
-    ]
-    return InlineKeyboardMarkup(buttons)
