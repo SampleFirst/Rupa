121d8b4621f81cab79266f55d10f1de514d0769a
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-11-24 12:25:53 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import youtube_dl
-
+from pyrogram.types import Message
 
 def get_song_info(song_name):
     # Use the youtube_dl library to search for the song on YouTube
@@ -23,7 +23,7 @@ def get_song_info(song_name):
         else:
             return None
 
-def download_song(song_id, message):
+async def download_song(song_id, message: Message):
     # Use the youtube_dl library to download the song
     ydl_opts = {
         'format': 'bestaudio/best',
@@ -32,8 +32,10 @@ def download_song(song_id, message):
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
+        'outtmpl': f'%(title)s.%(ext)s'  # Save the file with the title name
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         result = ydl.extract_info(song_id, download=True)
-        # Send the downloaded song as a reply to the user
-        await message.reply_audio(result['url'], title=result['title'], performer=result['uploader'])
+        file_path = f"{result['title']}.mp3"
+        # Reply with the downloaded song
+        await message.reply_audio(audio=file_path, title=result['title'], performer=result['uploader'])
